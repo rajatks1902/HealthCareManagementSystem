bfbd9cdfab9f268ffee34e6ea374c56e646c5887
rajatks1902 <rajatkumarsingh0707@gmail.com>
2025-09-03 21:09:02 +0530

    Added DockerManagement Micro

---
+++ doctorRepo/src/main/java/com/rajat/doctorRepo/DoctorRepoApplication.java
@@ -0,0 +1,13 @@
+package com.rajat.doctorRepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorRepoApplication.class, args);
+	}
+
+}
---
+++ doctorRepo/src/test/java/com/rajat/doctorRepo/DoctorRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajat.doctorRepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
