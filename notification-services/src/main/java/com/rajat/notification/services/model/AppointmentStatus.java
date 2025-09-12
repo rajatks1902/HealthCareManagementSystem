02cf7e01684db16603e06d29370b1c6881670e93
rajatks1902 <rajatkumarsingh0707@gmail.com>
2025-09-12 22:01:14 +0530

    Added Notification Service

---
+++ notification-services/src/main/java/com/rajat/notification/services/model/AppointmentStatus.java
@@ -0,0 +1,13 @@
+package com.rajat.notification.services.model;
+
+/**
+ * Enum to represent the various statuses an appointment can have.
+ */
+public enum AppointmentStatus {
+    PENDING,
+    CONFIRMED,
+    REJECTED,
+    COMPLETED;
+
+
+}
